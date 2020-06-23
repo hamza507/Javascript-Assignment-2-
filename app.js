174383487bc58edf365db525d5fa40abf6529065
hamza507 <65030926+hamza507@users.noreply.github.com>
2020-06-23 22:27:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,109 @@
+// // Chapter # 21-25 
+
+//  Q.1
+
+// var first = prompt("Enter your First Name");
+// var last = prompt("Enter your Last Name");
+// alert(first.concat(last))
+
+
+// Q.2 
+
+// var model = prompt("Enter your favorate Mobile Phone Model");
+// alert(model.length);
+
+// Q.3 
+
+// var name="Pakistani";
+// alert(name.indexOf("n"))
+
+// Q.4
+
+// var name="Hello World";
+// alert(name.lastIndexOf("l"))
+
+
+//  Q.5
+
+// var name="Pakistani";
+// alert(name.charAt(3))
+
+
+//  Q.6
+
+// var first = prompt("Enter your First Name");
+// var last = prompt("Enter your Last Name");
+// alert(first.concat(last))
+
+
+//Q.7
+
+// var name = "Hyderabad";
+// alert(name.replace("Hyder", "Islam"))
+
+
+//Q.8
+
+// var phrase = "Ali and Sami are best friends. They play cricket and football together";
+// alert(phrase.replace("and","&"))
+
+
+//Q.9
+
+// var num = 472;
+// alert(num.toString())
+
+
+// Q.10
+// var str = "peanuts";
+// alert(str.toUpperCase());
+
+
+
+// //Q.11
+
+// var str = "javscript";
+// alert(str.toTitleCase());
+
+
+// //Q.12
+
+// var num = 35.36;
+// alert(Math.round(num));
+
+
+
+// Q.17 
+
+// var firstChar = "Pakistan";
+// alert(firstChar.charAt(7))
+
+
+
+
+
+//Chapter # 26-30
+
+//Question # 1
+// var num=3.45214;
+// var round = Math.round(num);
+// var round= Math.floor(num);
+// var round = Math.ceil(num);
+// document.write(round);
+
+
+//Question # 2
+// var num= -2.673;
+// var round = Math.round(num);
+// var round= Math.floor(num);
+// var round = Math.ceil(num);
+// document.write(round);
+
+
+// //Question # 4
+// var a=Math.random(2);
+// document.write(a)
+// var b=Math.random(6);
+// document.write(b);
+
+
